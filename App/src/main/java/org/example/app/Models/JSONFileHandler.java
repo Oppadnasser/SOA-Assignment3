50acdac335d68454f40987b03b9633a4609302ae
Oppadnasser <oppadnasser@gmail.com>
2024-12-16 16:01:13 +0200

    modify update function

--- App/src/main/java/org/example/app/Models/JSONFileHandler.java
+++ App/src/main/java/org/example/app/Models/JSONFileHandler.java
@@ -13,7 +13,6 @@ public class JSONFileHandler {
 
         try {
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File(FILE_PATH), employees);
-            System.out.println("Data successfully written to " + FILE_PATH);
         } catch (IOException e) {
             System.err.println("Error writing to file: " + e.getMessage());
         }
